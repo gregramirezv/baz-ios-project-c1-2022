dc19f53e9972333e12c4ccfddd022bf75d135115
Gregorio Ramirez <luis.ramirezv@elektra.com.mx>
2022-10-18 15:56:16 -0500

    Create NewViewController

---
+++ BAZProjectC1/NewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewViewController.swift
+//  BAZProjectC1
+//
+//  Created by 1030364 on 18/10/22.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
